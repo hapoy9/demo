5a169c44bf32f5d1acce6108026f4d0661c4e3af
guoyongkui <gyk_hapoy@163.com>
2018-11-17 20:52:21 +0800

    修改日志配置 删除.idea和target目录的版本控制

--- demo/src/main/java/com/demo/controller/IndexController.java
+++ demo/src/main/java/com/demo/controller/IndexController.java
@@ -1,14 +1,16 @@
 package com.demo.controller;
 
 import com.jfinal.core.Controller;
+import org.apache.log4j.Logger;
 
 /**
- * @Auther: guoyongkui
- * @Date: 2018/11/9 22:26
- * @ProjectName: demo
- * @Description:
+ * @author: guoyongkui
+ * @date:  2018/11/9 22:26
+ * @projectName:  demo
+ * @description:
  */
 public class IndexController  extends Controller {
+    private static final Logger logger = Logger.getLogger(IndexController.class);
 
     public void index(){
         renderJson("汤迎春是吃货");
